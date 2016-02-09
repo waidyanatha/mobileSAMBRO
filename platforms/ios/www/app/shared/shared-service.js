8cdcd3a32b4ed638e81cc6bf99856c63de8a54ac
Angga Bayu <angga.bayu.marthafifsa@gmail.com>
2016-02-09 17:06:05 +0700

    platform build folder

---
+++ platforms/ios/www/app/shared/shared-service.js
@@ -0,0 +1,9 @@
+"use strict";
+
+angular.module("ngapp").service("shared", function(){
+
+    this.info = {
+        title: "cordova-angular-angularMaterial-seed",
+        auth: "Mario Aleo"
+    };
+});
