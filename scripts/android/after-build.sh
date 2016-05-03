be382eb53b436e5b320866a78ca4cb7d3df333ab
Lutz Frommberger <lutz@datenkollektiv.de>
2016-05-03 19:43:34 +0200

    overwriting output apk if needed, adding version number

--- scripts/android/after-build.sh
+++ scripts/android/after-build.sh
@@ -4,9 +4,19 @@ ZIPALIGN=/var/lib/jenkins/possum/android-sdk-linux/build-tools/20.0.0/zipalign
 #ZIPALIGN=/Users/lutz/Library/Android/sdk/build-tools/23.0.2/zipalign
 BUILDLOC=platforms/android/build/outputs/apk
 APK=android-release-unsigned.apk
+TARGETBASE=SAMBROmobile
+
+
+VERSION=`grep "version=" config.xml|grep -v xml |awk -F '"' '{print $2}'`
+
+TARGET=$TARGETBASE-$VERSION.apk
+
+if [ -f $BUILDLOC/$TARGET ]; then
+	rm $BUILDLOC/$TARGET
+fi
 
 if [ -f $ZIPALIGN ]; then
-	$ZIPALIGN -v 4 $BUILDLOC/$APK $BUILDLOC/SAMBROmobile-latest-release.apk
+	$ZIPALIGN -v 4 $BUILDLOC/$APK $BUILDLOC/$TARGET
 else
 	echo "ZIPALIGN not found"
 fi
